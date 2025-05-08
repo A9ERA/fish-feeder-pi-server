bba9e7ded80d7b99c2ffbdb599097881e0e9f0b7
TaeChanathip <chanathip.pla@builk.com>
2025-05-08 21:08:04 +0700

    Add functionality to handle incoming MQTT messages and post data to Firebase

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 import paho.mqtt.client as mqtt
+import requests
+import json
+
+FIREBASE_URL = "https://addsensorvalue-bp4gxqhmza-uc.a.run.app"
 
 # ฟังก์ชันเมื่อเชื่อมต่อกับ Broker สำเร็จ
 def on_connect(client, userdata, flags, rc):
@@ -8,6 +12,26 @@ def on_connect(client, userdata, flags, rc):
 # ฟังก์ชันเมื่อได้รับข้อความจาก ESP32
 def on_message(client, userdata, msg):
     print(f"Topic: {msg.topic}, Message: {msg.payload.decode()}")
+
+def on_message(client, userdata, msg):
+    try:
+        payload = msg.payload.decode()
+        print(f"MQTT Received: {payload}")
+        data_dict = json.loads(payload)  # แปลง JSON string เป็น dict
+
+        for key, value in data_dict.items():
+            post_data = {
+                "sensorName": key,
+                "value": value
+            }
+
+            headers = {'Content-Type': 'application/json'}
+            response = requests.post(FIREBASE_URL, headers=headers, data=json.dumps(post_data))
+            print(f"Sent {key}: {value} → {response.status_code}, {response.text}")
+
+    except Exception as e:
+        print("Error:", e)
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
